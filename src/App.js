5e0395695507b42043e512ea7b02a85c4b77bc15
ray221183 <b06901071@ntu.edu.tw>
2020-12-19 04:30:06 +0800

    modify topbanner 1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import { Button, Input, message, Tag } from 'antd'
 import TopBanner from './TopBanner/TopBanner'
 
 function App() {
+	const scrollYTop = useRef()
+	const [scrollToTop, setScrollToTop] = useState(false)
+	const scrolling = () => {
+		console.log(1)
+		const yPosition = scrollYTop.current.scrollTop
+		if( yPosition === 0 ) setScrollToTop(true)
+		else setScrollToTop(false)
+	}
+
 	return(
-    	<div className = "body">
-			<TopBanner />
+    	<div className = "body" ref={scrollYTop} onScroll = {() => {scrolling()}}>
+			<TopBanner scrollToTop={scrollToTop}/>
       		<div className = "App">
         		<div>
 					<h1>ss</h1>
