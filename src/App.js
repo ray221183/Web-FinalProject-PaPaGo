fb0f096e51c23973e6aed5b5c9b55bb1a4a725ee
ray221183 <b06901071@ntu.edu.tw>
2020-12-22 22:40:54 +0800

    add Home.js 2002/12/22 22:40 by ray

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css'
 import React, { useEffect, useRef, useState } from 'react'
-// import useChat from './useChat'
-import { Button, Input, message, Tag } from 'antd'
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 
-import TopBanner from './TopBanner/TopBanner'
-import Footer from './Footer/Footer'
-import LogPage from './LogPage/LogPage'
+import Home from './component/Home/Home'
+import TopBanner from './component/TopBanner/TopBanner'
+import Footer from './component/Footer/Footer'
+import LogPage from './component/LogPage/LogPage'
 
 function App() {
 	const scrollYTop = useRef();
 	const [scrollToTop, setScrollToTop] = useState(true);
 	const [loginLogup, setLoginLogup] = useState(0); //0:normal page | 1: log in | 2: log up
-	const [city, setCity] = useState('');
 
 	const scrolling = () => {
 		const yPosition = scrollYTop.current.scrollTop
@@ -19,47 +23,13 @@ function App() {
 		else setScrollToTop(false)
 	}
 
-	const clickCity = (event) => {
-		const name = event.target.name;
-		//console.log('name = ' + name);
-		//console.log('typeof(name) = ' + typeof(name))
-		setCity(name);
-	}
-
-	console.log(scrollToTop)
-
 	return(
     	<div className = "body" ref={scrollYTop} onScroll = {() => {scrolling()}}>
 			<div className = "TopElement">
-				<LogPage
-					loginLogup={loginLogup}
-					setLoginLogup={setLoginLogup}
-				/>
-				<TopBanner 
-					scrollToTop={scrollToTop}
-					setLoginLogup={setLoginLogup}
-				/>
+				<LogPage loginLogup={loginLogup} setLoginLogup={setLoginLogup} />
+				<TopBanner scrollToTop={scrollToTop} setLoginLogup={setLoginLogup} />
 			</div>
-
-			<section className = "App">
-				<div className = "App-main-picture">
-				</div>
-				<div>
-					<h1>你想去什麼地方</h1>
-				</div>
-				<div>
-					<button className="button" name="taipei" onClick={clickCity}>Taipei</button>
-					<button className="button" name="taoyuan" onClick={clickCity}>Taoyuan</button>
-					<button className="button" name="hsinchu" onClick={clickCity}>Hsinchu</button>
-					<button className="button" name="taichung" onClick={clickCity}>Taichung</button>
-					<button className="button" name="kaohsiung" onClick={clickCity}>Kaohsiung</button>
-					<button className="button" name="tainan" onClick={clickCity}>Tainan</button>
-				</div>
-				<div>
-					<iframe src="https://www.google.com/maps/d/embed?mid=1TGX6Qn2n5dsTALZGXVHs3crFe-iZkwZp" width="100%" height="400px"></iframe>
-				</div>
-			</section>
-
+			<Home />
 			<Footer />
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'antd/dist/antd.css'
+import {
+	BrowserRouter as Router,
+  } from "react-router-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
