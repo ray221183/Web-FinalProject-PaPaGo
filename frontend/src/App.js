0604f0e914e254b989dd790d303420116c149865
ray221183 <b06901071@ntu.edu.tw>
2021-01-16 14:45:40 +0800

    add_editor_3 2020/1/16 14:45 by ray

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import {
 	Switch,
 	Route,
   } from "react-router-dom";
-// import { createBrowserHistory } from 'history'
+  import { EditorState, convertToRaw, convertFromRaw } from 'draft-js'
+
 
 import Home from './component/Home/Home'
 import TopBanner from './component/TopBanner/TopBanner'
@@ -22,6 +23,7 @@ function App() {
 	const [loginLogup, setLoginLogup] = useState(0); //0:normal page | 1: log in | 2: log up
 	const [loginState, setLoginState] = useState(false);
 
+	const [editorState, setEditorState] = useState( ()=>EditorState.createEmpty() );
 	const [prePublishScale, setPrePublishScale] = useState(0);
 	
 	console.log(window.innerHeight, window.innerWidth)
@@ -35,7 +37,21 @@ function App() {
 		if( yPosition <= picHeight - topBannerHeight) setScrollToTop(true)
 		else setScrollToTop(false)
 	}
-
+	const savefile = (editorState, tags, published) => {
+        const contentState = editorState.getCurrentContent()
+		const jsonRawData = JSON.stringify(convertToRaw(contentState))
+		let tagList = ( tags.length>0 ) ? tags.map((item)=>{ return item[0] }) : ['']
+		let time = new Date()
+		let saveDate = time.getFullYear() + '/' + (time.getMonth() + 1) + '/' + time.getDate()
+		let saveTime = time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+		let dateTime = saveDate+' '+saveTime
+		console.log(jsonRawData, '    data type: ', typeof jsonRawData)
+		console.log(tagList)
+		console.log(dateTime)
+		console.log(published)
+	}
+	
+	
 	return(
     	<div className = "body" ref={scrollYTop} onScroll = {() => {scrolling()}}>
 			<div className = "TopElement">
@@ -44,8 +60,10 @@ function App() {
 					loginLogup={loginLogup} 
 					setLoginLogup={setLoginLogup} />
 				<PublishCheck 
+					editorState={editorState}
 					prePublishScale={prePublishScale}
 					setPrePublishScale={setPrePublishScale}
+					savefile={savefile}
 				/>
 				<TopBanner 
 					setLoginState={setLoginState} 
@@ -66,7 +84,10 @@ function App() {
 				/>
 				<Route path="/editor" render={() => (
 					<Editor 
+						editorState={editorState}
+						setEditorState={setEditorState}
 						setPrePublishScale={setPrePublishScale}
+						savefile={savefile}
 					/>)} 
 				/>
 			</Switch>
--- frontend/src/component/Editor/Editor.js
+++ frontend/src/component/Editor/Editor.js
@@ -1,29 +1,40 @@
 import React, { useEffect, useRef, useState } from 'react'
-import {EditorState} from 'draft-js'
 import EditorCore from './EditorCore'
 import './Editor.css'
+import './Publish.css'
 
 
 function Editor(prop){
-    const [editorState, setEditorState] = useState( ()=>EditorState.createEmpty() );
-
+    const [saved, setSaved] = useState(true);
     const changeScale = () => {
         console.log("prepare to publish")
         prop.setPrePublishScale(1)
     }
+
+    useEffect(() => {
+        setSaved(false)
+        const timer = setTimeout(() => {
+            setSaved(true)
+            prop.savefile(prop.editorState, [], false)
+        }, 2000)
+        return () => clearTimeout(timer)
+    }, [prop.editorState.getCurrentContent()])
 
     return(
         <section className = "Editor">
             <div className="editor-part" name = "editor-part">
                 <div className="top-part">
                     <div className="publish" onClick={changeScale}>
-                        <span>
+                        <span id="save-state">
+                            { (saved) ? 'saved' : 'saving...' }
+                        </span>
+                        <span id="publish-button">
                             Publish
                         </span>
                     </div>
                 </div>
                 <React.Fragment>
-                    <EditorCore editorState={editorState} setEditorState={setEditorState}/>
+                    <EditorCore editorState={prop.editorState} setEditorState={prop.setEditorState}/>
                 </React.Fragment>
             </div>
         </section>
@@ -33,14 +44,50 @@ function Editor(prop){
 function PublishCheck(prop){
     const elementParent = useRef();
     const elementChild = useRef();
+    const [curTag, setCurTag] = useState('');
+    const [tags, setTags] = useState([]);
+    const [tagId, setTagId] = useState(0);
     const scale = {
         transform: `scale(${prop.prePublishScale})`
     }
+
     const changeScale = (e) => {
-        console.log("close")
         prop.setPrePublishScale(0)
     }
-
+    const handleKeyDown = (e) => {
+        if(e.keyCode === 13){
+            let tagsTemp = [...tags, [curTag, tagId]]
+            setTagId(tagId + 1)
+            setTags(tagsTemp)
+            setCurTag('')
+        }
+    }
+    const changeInputTag = (e) => {
+        setCurTag(e.target.value)
+    }
+    const removeTag = (idx) => {
+        let tagsTemp = tags.filter((item) => {return item[1] !== idx})
+        if(tagsTemp.length === 0) setTagId(0)
+        setTags(tagsTemp)
+    }
+    const DisplayTags = () => {
+        return(
+            <div>
+                {tags.map(
+                    (item) => {
+                        console.log(item)
+                        return(
+                            <div key={item[1]} className="tag">
+                                <span>{item[0]}</span>
+                                <span onClick={() => removeTag(item[1])}>X</span>
+                            </div>
+                        )
+                    }
+                )}
+            </div>
+        )
+    }
+    
     useEffect(() => {
         elementParent.current.addEventListener('click', (e) => {
             if (e.target !== elementChild.current && e.target.contains(elementChild.current)) {
@@ -52,6 +99,22 @@ function PublishCheck(prop){
     return(
         <div className="prepublish-part" style={scale} ref={elementParent}>
             <div className="interface-part" ref={elementChild}>
+                <div className="top-banner">
+
+                </div>
+                <div className="content-part">
+                    <div className="tag-fill">
+                        <input placeholder="請輸入關鍵字" value={curTag} onKeyDown={handleKeyDown} onChange={changeInputTag}/>
+                    </div>
+                    <div className="tag-list">
+                        <DisplayTags />
+                    </div>
+                </div>
+                <div className="footer">
+                    <span className="publish-button" onClick={() => prop.savefile(prop.editorState, tags, true)}>
+                        Publish
+                    </span>
+                </div>
             </div>
         </div>
     )
