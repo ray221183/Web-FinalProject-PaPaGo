b028077854ee2ab335c5ab1eeccefe8840806f77
ray221183 <b06901071@ntu.edu.tw>
2021-01-18 22:17:54 +0800

    merge 2020/1/18 22:17

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const port = process.env.PORT || 80;
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-const bodyParser = require('body-parser')
-const apiRoute = require('./src/route/api');
-app.use('/api', apiRoute);
-app.use(bodyParser.json());
+// const bodyParser = require('body-parser')
+// const apiRoute = require('./src/route/api');
+// app.use('/api', apiRoute);
+// app.use(bodyParser.json());
 
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
+// app.get('/ping', function (req, res) {
+//   return res.send('pong');
+// });
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
