d0f419ebe33eece312a379c40daa0b556082d370
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-20 06:31:39 +0800

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import Query from '../server/resolvers/Query'
-import Mutation from '../server/resolvers/Mutation'
+//const { loadFile } = require('graphql-import-files')
 const {importSchema} = require('graphql-import')
 const { ApolloServer, gql } = require('apollo-server-express');
 const app = require('express')();
-const typeDefs = importSchema('../server/schema.graphql')
-const resolvers = {Query,Mutaion}
+const typeDefs = importSchema('./server/schema.graphql')
+const resolvers = require('./src/graphql/index.')
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 app.listen({ port: 80})
