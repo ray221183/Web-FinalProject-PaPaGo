40adba5db09d1a46c9dc3f429bb0e963f5424ee5
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-20 04:56:58 +0800

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 import { graphqlExpress } from 'apollo-server-express';
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 80;
+const app = express();
+const bodyParser = require('body-parser')
 const myGraphQLSchema = loadSchema('server/schema.graphql',{  // load from a single schema file
     loaders: [
         new GraphQLFileLoader()
     ]
 })// ... define or import your schema here!
-const PORT = 80;
 
-const app = express();
+
 
 // bodyParser is needed just for POST.
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
 console.log("apollo start")
-app.listen(PORT);
