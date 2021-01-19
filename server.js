16d36110e661d949725db2c45c579ba45301e244
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-20 06:08:49 +0800

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
-//import { graphqlExpress } from 'apollo-server-express';
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+import Query from './server/resolvers/Query'
+import Mutation from './server/resolvers/Mutation'
 const {importSchema} = require('graphql-import')
-const schema = importSchema('./server/schema.graphql')// ... define or import your schema here!
-const app = express();
-port = 80
-
-// bodyParser is needed just for POST.
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-// Start the server
-app.listen(80, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
-});
+const { ApolloServer, gql } = require('apollo-server-express');
+const app = require('express')();
+const typeDefs = importSchema('./server/schema.graphql')
+const resolvers = {Query,Mutaion}
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+app.listen({ port: 80})
