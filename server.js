6f9f7c07ce2ba8871f72776a77fe8010643e4879
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-20 08:16:26 +0800

    server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 //const { loadFile } = require('graphql-import-files')
-const {importSchema} = require('graphql-import')
-const { ApolloServer, gql } = require('apollo-server-express');
-const app = require('express')();
+import { USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY,
+ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT} from './src/graphql/index.js'
+import {importSchema} from'graphql-import'
+import {ApolloServer, gql } from 'apollo-server-express';
+import express from 'express'
+const app = express()
+console.log("qaq")
 const typeDefs = importSchema('./server/schema.graphql')
-const resolvers = require('./src/graphql/index.')
-const server = new ApolloServer({ typeDefs, resolvers });
+console.log("qaq2")
+const server = new ApolloServer({ typeDefs, resolvers:{ USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY,
+ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT} });
 server.applyMiddleware({ app });
 app.listen({ port: 80})
--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,3 +1,4 @@
-export { USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY } from './queries'
-export { ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT } from './mutations'
-export {} from './subscriptions'
+export { USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY } from './queries.js'
+export { ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT } from './mutations.js'
+export {} from './subscriptions.js'
+
