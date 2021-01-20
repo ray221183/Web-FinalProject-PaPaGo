df9d141b03a650795d7460cff2ef802bcedbd02b
Franklin905 <franklin94163@gmail.com>
2021-01-20 23:15:04 +0800

    modify style 2021/1/20 23:15 by Franklin

--- src/component/PersonalPage/PersonalPageStory.js
+++ src/component/PersonalPage/PersonalPageStory.js
@@ -13,11 +13,6 @@ export default ({ post, editEssay, deleteP, isPublic }) => {
 	        <div className="PPStories">
 				<div className="PPStoryTitle">
 					<Link to={"/post/" + `${post.uuid}`}><button className="PPStoryTitle">{post.title}</button></Link>
-					<IconContext.Provider value={{ size: '20px', style:{ fill: 'gray', marginLeft: '10px', marginBottom: '4px' } }}>
-						<button id="PPIconButton" onClick={() => deleteP(post.uuid)}>
-							<AiFillDelete />
-						</button>		
-					</IconContext.Provider>
 				</div>
 				<p className="PPStoryIntro">{post.introduction}</p>
 				<div className="PPDateAndTag">
@@ -26,6 +21,11 @@ export default ({ post, editEssay, deleteP, isPublic }) => {
 						<IconContext.Provider value={{ size: '16px', style:{ fill: 'black', marginLeft: '10px', marginBottom: '4px' } }}>
 							<FaThumbsUp />
 							<span id="PPLike">{post.great_num} Like</span>
+						</IconContext.Provider>
+						<IconContext.Provider value={{ size: '20px', style:{ fill: 'gray', marginLeft: '10px', marginBottom: '4px' } }}>
+							<button id="PPIconButton" onClick={() => deleteP(post.uuid)}>
+								<AiFillDelete />
+							</button>		
 						</IconContext.Provider>
 					</div>
 				</div>
