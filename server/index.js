1846ab9ab4aea63925f663d7f31f9b1daa538925
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-21 04:30:42 +0800

    deploy

--- server/index.js
+++ server/index.js
@@ -41,6 +41,6 @@ const server = new GraphQLServer({
   }
 })
 
-server.start({ port: process.env.PORT | 80 }, () => {
-  console.log(`The server is up on port ${process.env.PORT | 80}!`)
+server.start({ port: process.env.PORT | 4000 }, () => {
+  console.log(`The server is up on port ${process.env.PORT | 4000}!`)
 })
