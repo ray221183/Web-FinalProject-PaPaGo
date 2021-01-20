8ddb138c52f5b2768d42d6e7d55be12cb4166f7f
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-21 07:40:03 +0800

    deploy

--- src/index.js
+++ src/index.js
@@ -18,12 +18,12 @@ import {
 
 // Create an http link:
 const httpLink = new HttpLink({
-  uri: 'http://papagoisme.herokuapp.com:80/'
+  uri: 'https://papagoisme.herokuapp.com:80/'
 })
 
 // Create a WebSocket link:
 const wsLink = new WebSocketLink({
-  uri: `ws://papagoisme.herokuapp.com:80/`,
+  uri: `wss://papagoisme.herokuapp.com:80/`,
   options: { reconnect: true }
 })
 
