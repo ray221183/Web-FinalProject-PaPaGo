16186884e4a6f9fba7771df3ba7895d49f469068
leechungchia <[b06901074@ntu.edu.tw]>
2021-01-20 19:54:59 +0800

    update

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 //const { loadFile } = require('graphql-import-files')
-const{ USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY,
-ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT} = require('./src/graphql')
+const {USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY,
+ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT} = require('./src/graphql/index')
 const {importSchema} = require('graphql-import')
-const {ApolloServer, gql } =require('apollo-server-express');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const express = require('express')
 const GMR = require('graphql-merge-resolvers')
 const { mergeTypeDefs } = require('@graphql-tools/merge');
-const Query = require('./server/resolvers/Query.js')
-const Mutation = require('./server/resolvers/Mutation.js')
+const {Query} = require('./server/resolvers/Query')
+const {Mutation} = require('./server/resolvers/Mutation')
+const { makeExecutableSchema } = require('graphql-tools');
+port = 3000
+//const Mutation = require('./server/resolvers/Mutation.js')
 const app = express()
+console.log(USER_QUERY)
 console.log("qaq")
 const typeDefs= mergeTypeDefs([USER_QUERY, POST_QUERY, GREATOFPOST_QUERY, GREATOFUSER_QUERY, MULTIPOST_QUERY,
 ADD_USER, ADD_POST, DELETE_POST, UPDATE_POST, UPDATE_GREAT
@@ -18,7 +22,15 @@ console.log(typeDefs)
 console.log('qqqqqqqqqqqqq')
 console.log(resolvers)
 console.log("qaq3")
-const server = new ApolloServer({ typeDefs, resolvers});
-console.log("qaq2")
-server.applyMiddleware({ app });
-app.listen({ port: 80})
+const schema = makeExecutableSchema({
+  ,
+  resolvers
+});
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.listen(port, () => {
+  console.log('Go to http://localhost:3000/graphiql to run queries!');
+});
