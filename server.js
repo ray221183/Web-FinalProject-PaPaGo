1d67abf32ccf1b7a678a7dc7315b320496a73c6f
Franklin905 <franklin94163@gmail.com>
2021-01-24 15:34:08 +0800

    2021/1/24 15:35

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 80;
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
+const { readFileSync } = require('fs');
 const bodyParser = require('body-parser')
-const apiRoute = require('./src/route/api');
-app.use('/api', apiRoute);
+const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express')
+const Query = require('./server/resolvers/Query')
+const Mutation = require('./server/resolvers/Mutation')
+const PORT = process.env.PORT || 80
+const app = express()
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
-
-app.get('/ping', function (req, res) {
-  return res.send('pong');
+const server = new ApolloServer({ typeDefs:readFileSync('./server/schema.graphql','utf-8'), resolvers:{
+	Query,
+	Mutation
+	},
+  //context:({req, res})=>({req,res}) 
 });
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
-console.log("Server Ready!")
+
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () =>
+  console.log("ready")
+);
