6f2abdfedf1fca4cf975e773811b6901ead95539
ray221183 <b06901071@ntu.edu.tw>
2021-01-24 16:56:36 +0800

    16:56

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const { readFileSync } = require('fs');
 const bodyParser = require('body-parser')
+const { readFileSync } = require('fs');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express')
@@ -7,6 +7,17 @@ const Query = require('./server/resolvers/Query')
 const Mutation = require('./server/resolvers/Mutation')
 const PORT = process.env.PORT || 80
 const app = express()
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://PaPaGo:papago123456@cluster0.4ksx8.mongodb.net/cluster0?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', (error) => {
+  console.error(error)
+})
+
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 const server = new ApolloServer({ typeDefs:readFileSync('./server/schema.graphql','utf-8'), resolvers:{
@@ -22,6 +33,9 @@ app.get('/*', function (req, res) {
 
 
 server.applyMiddleware({ app });
-app.listen({ port: PORT }, () =>
+db.once('open', () => {
+  console.log('MongoDB connected!')
+  app.listen({ port: PORT }, () =>
   console.log("ready")
 );
+})
